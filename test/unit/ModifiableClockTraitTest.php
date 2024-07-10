04d629dee1dc0ee01a1cc59c2865bac5affcd329
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2024-07-10 16:27:38 +0200

    qa: add some more tests regarding modifiable clock trait
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/unit/ModifiableClockTraitTest.php
+++ test/unit/ModifiableClockTraitTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace LaminasTestTest\Cache\Storage\Adapter;
 
+use DateTimeInterface;
 use LaminasTest\Cache\Storage\Adapter\ModifiableClockTrait;
 use PHPUnit\Framework\TestCase;
 
@@ -15,5 +16,23 @@ final class ModifiableClockTraitTest extends TestCase
     {
         $clock = $this->getClock();
         self::assertSame($clock, $this->getClock());
+    }
+
+    public function testAdvanceTimeCanBeCalledWithoutInitializingClock(): void
+    {
+        self::assertNull($this->modifiableClock);
+        $this->advanceTime(10);
+    }
+
+    public function testAdvanceTimePassesSecondsToModifiableClock(): void
+    {
+        $clock = $this->getClock();
+        $time  = $clock->now();
+
+        $this->advanceTime(10);
+        self::assertSame(
+            $time->modify('10 seconds')->format(DateTimeInterface::ATOM),
+            $clock->now()->format(DateTimeInterface::ATOM),
+        );
     }
 }
